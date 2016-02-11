9c62212630590c3813ecae053d50ea25bec35c4a
unknown <naofum@gmail.com>
2016-02-11 11:54:19 +0900

    Updated exchanges.js

--- exchanges.js
+++ exchanges.js
@@ -332,6 +332,21 @@ var exchanges = [
     requires: ['key', 'secret', 'username'],
     providesHistory: false
   },
+  {
+    name: 'Zaif',
+    slug: 'zaif.jp',
+    direct: false,
+    infinityOrder: false,
+    currencies: ['JPY'],
+    assets: ['BTC'],
+    markets: [
+      {
+        pair: ['JPY', 'BTC'], minimalOrder: { amount: 0.01, unit: 'asset' }
+      }
+    ],
+    requires: ['key', 'secret'],
+    providesHistory: false
+  },
 ];
 
 module.exports = exchanges;
